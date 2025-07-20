4bec24455234bf1784918b33a2515ebf5f072a33
Stephen Feest <steve84@gmail.com>
2025-07-20 17:25:26 +0100

    Add fonts
    I've used `font-display: optional` because I want to avoid delaying the
    rendering of the page while the fonts are downloaded, and also avoid the
    flash of unstyled text or layout shift that can occur if the fonts are
    swapped after the initial render.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,23 @@
 import { Viewport } from "next";
+import { Lora, Roboto } from "next/font/google";
 import "./globals.css";
 
 export const viewport: Viewport = {
   themeColor: "#2c6877",
 };
+
+const lora = Lora({
+  display: "optional",
+  fallback: ["serif"],
+  preload: false,
+  variable: "--font-lora",
+});
+const roboto = Roboto({
+  display: "optional",
+  fallback: ["sans-serif"],
+  preload: false,
+  variable: "--font-roboto",
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${lora.variable} ${roboto.variable}`}>
         <div className="pageContent">{children}</div>
       </body>
     </html>
