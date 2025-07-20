711de2c317721ff8786b9d6be9fe89f3a9ff7041
Stephen Feest <steve84@gmail.com>
2025-07-20 17:10:59 +0100

    Add page titles
    I had to move the home page into a route group as title templates are
    only applied to child segments [1].
    
    [1] https://nextjs.org/docs/app/api-reference/functions/generate-metadata#template

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from "next";
 import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 import styles from "./not-found.module.css";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("notFound");
+
+  return {
+    title: t("title"),
+  };
+}
 
 export default function NotFound() {
   const t = useTranslations("notFound");
