84295b1a3f2b971bd347b8c885bba073bec6927f
Stephen Feest <steve84@gmail.com>
2025-07-27 14:48:01 +0100

    Add website icons
    icon16.svg is the source file for the 16px icon; all other sizes are
    generated from icon.svg. All PNGs were losslessly compressed using
    Trimage [1].
    
    [1] https://trimage.org/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import icon128 from "./icon-128.png";
+import icon16 from "./icon-16.png";
+import icon32 from "./icon-32.png";
+import icon512 from "./icon-512.png";
+import icon96 from "./icon-96.png";
 
 export default function RootLayout({
   children,
@@ -7,6 +12,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href={icon16.src} sizes="16x16" />
+        <link rel="icon" type="image/png" href={icon32.src} sizes="32x32" />
+        <link rel="icon" type="image/png" href={icon96.src} sizes="96x96" />
+        <link rel="icon" type="image/png" href={icon128.src} sizes="128x128" />
+        <link rel="icon" type="image/png" href={icon512.src} sizes="512x512" />
+      </head>
       <body>{children}</body>
     </html>
   );
