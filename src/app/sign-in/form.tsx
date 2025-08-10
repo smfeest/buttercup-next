08742f18eff1fbfc6d41593e3005afec52618290
Stephen Feest <steve84@gmail.com>
2025-08-10 17:35:01 +0100

    WIP add form field components

--- src/app/sign-in/form.tsx
+++ src/app/sign-in/form.tsx
@@ -2,6 +2,7 @@
 
 import { useTranslations } from "next-intl";
 import { useActionState } from "react";
+import { InputField } from "../components/forms/fields";
 import ValidationSummary from "../components/forms/validation-summary";
 import { signIn } from "./actions";
 
@@ -11,14 +12,19 @@ export default function SignInForm() {
   return (
     <form action={formAction}>
       <ValidationSummary errors={errors} />
-      <label>
-        {t("emailFieldLabel")}
-        <input name="email" type="email" defaultValue={email} required />
-      </label>
-      <label>
-        {t("passwordFieldLabel")}
-        <input name="password" type="password" required />
-      </label>
+      <InputField
+        name="email"
+        label={t("emailFieldLabel")}
+        type="email"
+        defaultValue={email}
+        required
+      />
+      <InputField
+        name="password"
+        label={t("passwordFieldLabel")}
+        type="password"
+        required
+      />
       <button disabled={pending}>{t("signInButtonLabel")}</button>
     </form>
   );
