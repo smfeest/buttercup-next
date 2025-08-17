9e9e215f9a8ecadcbc39f029145e396adb5628d4
Stephen Feest <steve84@gmail.com>
2025-08-17 22:55:16 +0100

    WIP Add method to get session payload
    TODO: Unit tests

--- src/session/index.ts
+++ src/session/index.ts
@@ -1,10 +1,16 @@
 import "server-only";
 
 import { cookies } from "next/headers";
-import { encrypt } from "./encryption";
+import { decrypt, encrypt } from "./encryption";
 import { Payload } from "./payload";
 
 const cookieName = "buttercup-session";
+
+export async function getSession() {
+  const { get } = await cookies();
+  const cookie = get(cookieName);
+  return cookie ? await decrypt(cookie.value) : null;
+}
 
 export async function setSession({
   user: { id, name, email, isAdmin, timeZone },
