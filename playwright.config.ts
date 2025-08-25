068c2157da609fa29629c97cc3e4ca56811db243
Stephen Feest <steve84@gmail.com>
2025-08-25 17:08:29 +0100

    Use default reporters for end-to-end tests
    The config file created by the Playwright initializer specifies the
    `html` reporter. However I think Playwright's default behaviour [1] of
    using the `dots` reporter in CI and `list` reporter everywhere else is
    more practical.
    
    [1] https://playwright.dev/docs/test-reporters#list-reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : "50%",
-  reporter: "html",
   use: {
     baseURL,
     locale: "en-GB",
